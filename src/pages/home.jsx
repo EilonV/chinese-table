65389e86cb755a68ce1acd197269b579f6b6f454
EilonV <73593619+EilonV@users.noreply.github.com>
2023-06-29 16:22:57 +0300

    added gender color

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -132,15 +132,15 @@ export const Home = () => {
         else {
             day = result
             // console.log(day);
-            // if (day === 28)
-            //     day = 0
+            if (day === 28)
+                day = 0
         }
 
-        // console.log(`a/m: ${day + 1}/${month + 1}`);
+        // console.log(`a/m: ${day}/${month}`);
         // console.log('boy/girl: ', chart[day][month]);
 
         if (chart[day][month] === 'G') {
-            console.log('GIRL');
+            // console.log('GIRL');
             boy.classList.remove('active')
             girl.classList.add('active');
         }
